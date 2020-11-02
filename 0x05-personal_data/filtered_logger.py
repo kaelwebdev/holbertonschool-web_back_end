7a2d89e8a5219b7b237700f151f79749a94c0160
kaelwebdev <cdcp-1795@hotmail.com>
2020-11-02 00:40:37 -0500

    [Fix] filter_datum should be less than 5 lines long

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -24,13 +24,7 @@ class RedactingFormatter(logging.Formatter):
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """
-        returns the log message obfuscated
-        group(1) == key
-        group(0) == key=value
-        then
-        group(1) == key=redaction
-    """
+    """ returns the log message obfuscated """
     return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
                   lambda match: match.group(1) + "=" + redaction
                   if match.group(1) in fields else match.group(0), message)
