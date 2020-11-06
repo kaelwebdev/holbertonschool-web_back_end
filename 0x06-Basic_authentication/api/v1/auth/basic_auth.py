b99e9304ac461fcee7524ae163754c77d3a3fd25
kaelwebdev <cdcp-1795@hotmail.com>
2020-11-06 14:44:50 -0500

    [Fix] imports

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,8 @@ Basic authentication
 """
 from api.v1.auth.auth import Auth
 from base64 import b64decode
-
+from typing import TypeVar
+from models.user import User
 
 class BasicAuth(Auth):
     """
