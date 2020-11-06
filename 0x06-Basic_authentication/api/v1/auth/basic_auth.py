374872c7409fe1ce4f434b89ccf752b97ca19ae5
kaelwebdev <cdcp-1795@hotmail.com>
2020-11-06 14:02:03 -0500

    a function is implemented that manages to extract the authentication value

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,8 @@ class BasicAuth(Auth):
     """
     basic authentication
     """
-    pass
+    def extract_base64_authorization_header(self, auth_header: str) -> str:
+        """ return Base64 part of auth header for a basic authentication. """
+        if auth_header is None or type(auth_header) is not str:
+            return None
+        return auth_header[6:] if auth_header.startswith('Basic ') else None
