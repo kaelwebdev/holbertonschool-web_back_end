d304d3f01eb799bde2e9989d4b54d294337ea160
kaelwebdev <cdcp-1795@hotmail.com>
2020-11-06 14:13:02 -0500

    [Update] a function is implemented that manages to decode information in base 64

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Basic authentication
 """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -14,3 +15,15 @@ class BasicAuth(Auth):
         if auth_header is None or type(auth_header) is not str:
             return None
         return auth_header[6:] if auth_header.startswith('Basic ') else None
+
+    def decode_base64_authorization_header(self, b64_auth_header: str) -> str:
+        """ return decoded value of a Base64 string. """
+        if b64_auth_header is None or type(b64_auth_header) is not str:
+            return None
+        try:
+            h = b64_auth_header.encode('utf-8')
+            h = b64decode(h)
+            h = h.decode('utf-8')
+            return h
+        except BaseException:
+            return None
