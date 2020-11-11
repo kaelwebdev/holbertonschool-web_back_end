d6a265b01d6e16d17877ebbd617ecf99b92cafe6
kaelwebdev <cdcp-1795@hotmail.com>
2020-11-11 13:32:25 -0500

    Create a session

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -7,6 +7,15 @@ from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
     """
-    empty class
+    session authentication class
     """
-    pass
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a Session ID for a user_id
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        s_id = str(uuid.uuid4())
+        self.user_id_by_session_id[s_id] = user_id
+        return s_id
