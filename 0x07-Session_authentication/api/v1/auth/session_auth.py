e1a9a1f8c1b93be4645a246e34a35610be4bc175
kaelwebdev <cdcp-1795@hotmail.com>
2020-11-11 19:24:49 +0000

    User ID from Session ID

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@ session authentication
 from api.v1.auth.auth import Auth
 import uuid
 from typing import TypeVar
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -22,3 +23,11 @@ class SessionAuth(Auth):
         s_id = str(uuid.uuid4())
         self.user_id_by_session_id[s_id] = user_id
         return s_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        returns a User ID based on a Session ID:
+        """
+        if session_id is None or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id, None)
