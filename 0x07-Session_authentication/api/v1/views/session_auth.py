8d7efd230f290501493e665418f4ccc635266f3e
kaelwebdev <cdcp-1795@hotmail.com>
2020-11-11 15:55:33 -0500

    [Create] New view for Session Authentication

---
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+session authentication - view
+"""
+from flask import jsonify, request, abort
+from models.user import User
+from api.v1.views import app_views
+from os import getenv
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    send and check login data
+    """
+    u_email = request.form.get('email')
+    if not u_email:
+        return jsonify({"error": "email missing"}), 400
+
+    u_pwd = request.form.get('password')
+    if not u_pwd:
+        return jsonify({"error": "password missing"}), 400
+
+    try:
+        users = User.search({"email": u_email})
+    except BaseException:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for u in users:
+        if not u.is_valid_password(u_pwd):
+            return jsonify({"error": "wrong password"}), 401
+        from api.v1.app import auth
+        s_id = auth.create_session(u.id)
+        r = jsonify(u.to_json())
+        r.set_cookie(getenv('SESSION_NAME'), s_id)
+        return r
+    return jsonify({"error": "no user found for this email"}), 404
