a5ea97614ff2cd906d35fe0477638b3c9983ad25
kaelwebdev <cdcp-1795@hotmail.com>
2020-11-19 02:11:14 +0000

    Credentials validation

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -41,3 +41,13 @@ class Auth:
             pass
         n_user = self._db.add_user(email, _hash_password(password))
         return n_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        check password and return true || false
+        """
+        try:
+            u = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), u.hashed_password)
+        except NoResultFound:
+            return False
