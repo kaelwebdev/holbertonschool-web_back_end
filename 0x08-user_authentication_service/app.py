5ecd4d83e92aa70eb3e2edfd66b5e8924850598a
kaelwebdev <cdcp-1795@hotmail.com>
2020-11-20 14:22:40 +0000

    [Fix] type of request corrected

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -89,7 +89,7 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
-@app.route('/reset_password', methods=["POST"], strict_slashes=False)
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password() -> str:
     """
     Update password for user endpoint
