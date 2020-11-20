5a822a3ef475e7979d8ba967075cbed75103d973
kaelwebdev <cdcp-1795@hotmail.com>
2020-11-20 14:28:43 +0000

    [Fix] fixed server response status

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -99,7 +99,7 @@ def update_password() -> str:
     new_pwd = request.form.get("new_password")
     try:
         AUTH.update_password(token, new_pwd)
-        return jsonify({"email": email, "message": "Password updated"}), 403
+        return jsonify({"email": email, "message": "Password updated"}), 200
     except Exception:
         abort(403)
 
