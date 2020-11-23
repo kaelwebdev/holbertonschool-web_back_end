9c303582300975d5ee80d1d360ffd997f4d9c60d
kaelwebdev <cdcp-1795@hotmail.com>
2020-11-23 09:25:30 +0000

    start p0x09-Unittests_and_integration_tests

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Tests for utils
+"""
+
+from unittest import TestCase
+from parameterized import parameterized
+from utils import requests, get_json, access_nested_map, memoize
+
+
+class TestAccessNestedMap(TestCase):
+    """ Test cases for utils.test_access_nested_map """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, values, path, expected):
+        """
+        Testing return
+        """
+        r = access_nested_map(values, path)
+        self.assertEqual(r, expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
+    def test_access_nested_map_errors(self, values, path, expected):
+        """
+        Testing errors
+        """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(values, path)
+            self.assertEqual(expected, str(e.exception))
