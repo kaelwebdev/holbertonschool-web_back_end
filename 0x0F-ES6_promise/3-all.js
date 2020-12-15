f47640b840ba23c6ee2cc081678b1e0292c1a3f8
kaelwebdev <cdcp-1795@hotmail.com>
2020-12-15 22:58:40 +0000

    backtracking changes but with an improvement

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,17 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return uploadPhoto()
-    .then(({ body }) => {
-      createUser()
-        .then(({ firstName, lastName }) => {
-          console.log(`${body} ${firstName} ${lastName}`);
-        })
-        .catch(() => {
-          console.log('Signup system offline');
-        });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((r) => {
+      console.log(`${r[0].body} ${r[1].firstName} ${r[1].lastName}`);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
