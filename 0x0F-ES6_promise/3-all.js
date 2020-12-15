bf342cda17b1f51b492b41c45d836958b2cca75b
kaelwebdev <cdcp-1795@hotmail.com>
2020-12-15 22:26:33 +0000

    Handle multiple successful promises

---
+++ 0x0F-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((r) => {
+      console.log(r[0].body, r[1].firstName, r[1].lastName);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
