821530df555b6ad2a5a039de13df3cc45132c2e2
kaelwebdev <cdcp-1795@hotmail.com>
2020-12-21 15:58:02 +0000

    .

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -13,9 +13,6 @@ export class HolbertonClass {
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -32,6 +29,8 @@ export class StudentHolberton {
   }
 }
 
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
