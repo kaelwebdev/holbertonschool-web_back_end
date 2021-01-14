191a17c35ac6afc46afca124db62740ee7292616
kaelwebdev <cdcp-1795@hotmail.com>
2021-01-14 03:19:41 +0000

    more tests

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,16 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-    it('correctly adds 1 and 3', () => {
-        assert.equal(calculateNumber(1, 3), 4)
-    })
+  it('correctly adds 1 and 3', () => {
+    assert.equal(calculateNumber(1, 3), 4)
+  })
+  it('correctly adds 1 and 3.7', () => {
+    assert.equal(calculateNumber(1, 3.7), 5)
+  })
+  it('correctly adds 1.2 and 3.7', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5)
+  })
+  it('correctly adds 1.5 and 3.7', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6)
+  })
 })
