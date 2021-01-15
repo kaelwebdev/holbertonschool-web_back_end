1c3eda8762918c2b51f1ede58aefbf870cf8088b
kaelwebdev <cdcp-1795@hotmail.com>
2021-01-15 20:38:45 +0000

    cleaning test

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -17,20 +17,8 @@ describe('calculateNumber', () => {
   it('1.1 + 1.2', () => {
     assert.equal(calculateNumber('SUM', 1.1, 1.2), 2)
   })
-  it('1 - 3', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1, 3), -2)
-  })
-  it('1 - 3.7', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1, 3.7), -3)
-  })
-  it('1.2 - 3.7', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1.2, 3.7), -3)
-  })
-  it('1.5 - 3.7', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1.5, 3.7), -2)
-  })
-  it('1.1 - 1.2', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1.1, 1.2), 0)
+  it('1.4 - 4.5', () => {
+    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4)
   })
   it('1.4 / 4.5', () => {
     assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2)
