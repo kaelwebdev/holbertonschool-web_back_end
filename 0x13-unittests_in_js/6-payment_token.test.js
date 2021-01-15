49d93eb2f03d80fdfd023ebc846c63bc4679b84e
kaelwebdev <cdcp-1795@hotmail.com>
2021-01-15 22:10:54 +0000

    Async test

---
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+const getPaymentTokenFromApi = require('./6-payment_token.js');
+
+describe('getPaymentTokenFromAPI', () => {
+    it('true is passed', (done) => {
+        getPaymentTokenFromApi(true).then(function(r) {
+            expect(r).to.eql({ data: 'Successful response from the API' })
+        })
+        done();
+    })
+})
