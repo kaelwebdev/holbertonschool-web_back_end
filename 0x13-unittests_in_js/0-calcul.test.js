db665585e9b3268e4aea47aead9fd8a4803d4092
kaelwebdev <cdcp-1795@hotmail.com>
2021-01-15 19:25:03 +0000

    task 1

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -14,24 +14,6 @@ describe('calculateNumber', () => {
   it('correctly adds 1.5 and 3.7', () => {
     assert.equal(calculateNumber(1.5, 3.7), 6)
   })
-  it('negative number + positive number', () => {
-    assert.equal(calculateNumber(-1.5, 1.5), 1)
-  })
-  it('negative number + negative number', () => {
-    assert.equal(calculateNumber(-1, -1), -2)
-  })
-  it('zero + zero', () => {
-    assert.equal(calculateNumber(0, 0), 0)
-  })
-  it('2.999 + 1', () => {
-    assert.equal(calculateNumber(2.99, 1), 4)
-  })
-  it('1.1 + 0', () => {
-    assert.equal(calculateNumber(1.1, 0), 1)
-  })
-  it('-0.19 + 0', () => {
-    assert.equal(calculateNumber(-0.19, 0), 0)
-  })
   it('1.1 + 1.2', () => {
     assert.equal(calculateNumber(1.1, 1.2), 2)
   })
---
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul.js');
+
+describe('calculateNumber', () => {
+  it('1 + 3', () => {
+    assert.equal(calculateNumber(1, 3), 4)
+  })
+  it('1 + 3.7', () => {
+    assert.equal(calculateNumber(1, 3.7), 5)
+  })
+  it('1.2 + 3.7', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5)
+  })
+  it('1.5 + 3.7', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6)
+  })
+  it('1.1 + 1.2', () => {
+    assert.equal(calculateNumber(1.1, 1.2), 2)
+  })
+})
