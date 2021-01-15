0adfa7f58658a2e5d5b6c414e31dba4310bfc0e7
kaelwebdev <cdcp-1795@hotmail.com>
2021-01-15 21:25:33 +0000

    spy test

---
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -0,0 +1,17 @@
+const sinon = require('sinon');
+const { expect } = require('chai')
+const sendPaymentRequestToApi = require('./3-payment.js');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+  
+    it('check arguments (SUM, 100, 20) and call', () => {
+        sendPaymentRequestToApi(100, 20);
+        expect(spy.calledOnce).to.be.true;
+        expect(spy.args[0][0]).to.equal('SUM');
+        expect(spy.args[0][1]).to.equal(100);
+        expect(spy.args[0][2]).to.equal(20);
+        spy.restore();
+    })
+})
---
+++ 0x13-unittests_in_js/utils.js
@@ -0,0 +1,14 @@
+class Utils {
+  static calculateNumber(type, a, b) {
+    const ra = Math.round(a);
+    const rb = Math.round(b);
+ 
+    if (type === 'SUM') return ra + rb;
+
+    else if (type === 'SUBTRACT') return ra - rb;
+
+    else if (type === 'DIVIDE')
+      return rb === 0 ? 'Error' : ra / rb;
+  }
+}
+module.exports = Utils;
