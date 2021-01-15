900fdcd10e81bb737c89a4c5ac6908d9b64c314d
kaelwebdev <cdcp-1795@hotmail.com>
2021-01-15 19:59:42 +0000

    +tests

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -17,4 +17,20 @@ describe('calculateNumber', () => {
   it('1.1 + 1.2', () => {
     assert.equal(calculateNumber('SUM', 1.1, 1.2), 2)
   })
+  it('1 - 3', () => {
+    assert.equal(calculateNumber('SUBTRACT', 1, 3), 4)
+  })
+  it('1 - 3.7', () => {
+    assert.equal(calculateNumber('SUBTRACT', 1, 3.7), 5)
+  })
+  it('1.2 - 3.7', () => {
+    assert.equal(calculateNumber('SUBTRACT', 1.2, 3.7), 5)
+  })
+  it('1.5 - 3.7', () => {
+    assert.equal(calculateNumber('SUBTRACT', 1.5, 3.7), 6)
+  })
+  it('1.1 - 1.2', () => {
+    assert.equal(calculateNumber('SUBTRACT', 1.1, 1.2), 2)
+  })
+
 })
