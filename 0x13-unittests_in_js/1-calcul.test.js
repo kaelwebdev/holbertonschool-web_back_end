50f4734f96d90caf2a4264842ab415e4d8f384a4
kaelwebdev <cdcp-1795@hotmail.com>
2021-01-15 20:03:02 +0000

    [Fix] tests

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -18,19 +18,19 @@ describe('calculateNumber', () => {
     assert.equal(calculateNumber('SUM', 1.1, 1.2), 2)
   })
   it('1 - 3', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1, 3), 4)
+    assert.equal(calculateNumber('SUBTRACT', 1, 3), -2)
   })
   it('1 - 3.7', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1, 3.7), 5)
+    assert.equal(calculateNumber('SUBTRACT', 1, 3.7), -3)
   })
   it('1.2 - 3.7', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1.2, 3.7), 5)
+    assert.equal(calculateNumber('SUBTRACT', 1.2, 3.7), -3)
   })
   it('1.5 - 3.7', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1.5, 3.7), 6)
+    assert.equal(calculateNumber('SUBTRACT', 1.5, 3.7), -2)
   })
   it('1.1 - 1.2', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1.1, 1.2), 2)
+    assert.equal(calculateNumber('SUBTRACT', 1.1, 1.2), 0)
   })
 
 })
