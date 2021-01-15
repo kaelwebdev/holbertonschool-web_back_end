9ae38ce7d08d7ac2ef9fac92805f274e784e115f
kaelwebdev <cdcp-1795@hotmail.com>
2021-01-15 19:31:17 +0000

    [Fix] test

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -3,18 +3,18 @@ const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber', () => {
   it('1 + 3', () => {
-    assert.equal(calculateNumber(1, 3), 4)
+    assert.equal(calculateNumber('SUM', 1, 3), 4)
   })
   it('1 + 3.7', () => {
-    assert.equal(calculateNumber(1, 3.7), 5)
+    assert.equal(calculateNumber('SUM', 1, 3.7), 5)
   })
   it('1.2 + 3.7', () => {
-    assert.equal(calculateNumber(1.2, 3.7), 5)
+    assert.equal(calculateNumber('SUM', 1.2, 3.7), 5)
   })
   it('1.5 + 3.7', () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6)
+    assert.equal(calculateNumber('SUM', 1.5, 3.7), 6)
   })
   it('1.1 + 1.2', () => {
-    assert.equal(calculateNumber(1.1, 1.2), 2)
+    assert.equal(calculateNumber('SUM', 1.1, 1.2), 2)
   })
 })
